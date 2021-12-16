8983a7f817eb3437ec608a9d7ab0dbba3900327f
Kamlesh Prajapati <kamleshprajapati@Kamleshs-MacBook-Air.local>
2021-12-16 00:15:41 -0500

    add more functionality on website with help of js and organize html and css accordingly

--- app.js
+++ app.js
@@ -1,63 +1,66 @@
+// Global object 
+const myApp = {};
 
-// ================
-// hamgurger button
-const menuApp = {};
-
-menuApp.listOfMenu = document.querySelector('.menu-item');
-
-menuApp.menuDiv = document.querySelector('.menu');
-menuApp.hamburgerMenu = document.createElement('div');
-menuApp.hamburgerMenu.innerHTML = `<span class="hamburger-btn"><i class="fas fa-bars"></i></span>`;
-menuApp.menuDiv.appendChild(menuApp.hamburgerMenu);
-
-// ===============
-menuApp.hamburgerButton = document.querySelector('.fa-bars');
-menuApp.hamburgerClick = function(){
-menuApp.hamburgerButton.addEventListener('click', function(){
-    menuApp.listOfMenu.classList.toggle('active');
-});
+myApp.init = () => {
+    myApp.hamburgerClick();  
 }
-menuApp.hamburgerClick();
+// add hamburger button through js and add functionality: click pop menu bar in samll screen
+// create a varibale and store html selector
+myApp.listOfMenu = document.querySelector('.menu-item');
+myApp.menuDiv = document.querySelector('.menu');
 
+//  create div, inside div add hamburgerbutton
+myApp.hamburgerMenu = document.createElement('div');
+myApp.hamburgerMenu.innerHTML = `<span class="hamburger-btn"><i class="fas fa-bars"></i></span>`;
+myApp.menuDiv.appendChild(myApp.hamburgerMenu);
+myApp.hamburgerButton = document.querySelector('.fa-bars');
 
-// =====================
-// scroll to top button
+// add click eventlistner/ on click menu will open 
+myApp.hamburgerClick = function(){
+myApp.hamburgerButton.addEventListener('click', function(){
+    myApp.listOfMenu.classList.toggle('active');
+    });
+}
 
-const ScrollToTop = {};
-// select element in to variable
-ScrollToTop.heroPage = document.querySelector('.hero-page');
-ScrollToTop.rootElement = document.documentElement;
+// go to top button. create a variable and store selector 
+myApp.heroPage = document.querySelector('.hero-page');
+myApp.rootElement = document.documentElement;
 
-// add top font-awesome button on the html 
-ScrollToTop.topButton = `<div class = "top-btn"> <span ><a href="#home" class='go-to-top-link '><i class="show fas fa-chevron-circle-up"></i></a></span></div>`;
-ScrollToTop.heroPage.insertAdjacentHTML('afterend', ScrollToTop.topButton);
+// add top font-awesome button on the html by creating element 
+myApp.topButton = `<div class = "top-btn"> <span ><a href="#home" class='go-to-top-link '><i class="show fas fa-chevron-circle-up"></i></a></span></div>`;
+myApp.heroPage.insertAdjacentHTML('afterend', myApp.topButton);
 
-ScrollToTop.fontAwesome = document.querySelector('.fa-chevron-circle-up')
+myApp.fontAwesome = document.querySelector('.fa-chevron-circle-up')
 
-ScrollToTop.scrollUp = function(){
-    if (ScrollToTop.rootElement.scrollTop > 200) {
-        ScrollToTop.fontAwesome.classList.remove("show")
+// create condtion in function if window will move up after some height the button will appear 
+myApp.scrollUp = function(){
+    if (myApp.rootElement.scrollTop > 200) {
+        myApp.fontAwesome.classList.remove("show")
     }
     else {
-        ScrollToTop.fontAwesome.classList.add("show")
+        myApp.fontAwesome.classList.add("show")
     }
 }
-window.addEventListener('scroll', ScrollToTop.scrollUp);
+// declare function in add an event listner to run functionality
+window.addEventListener('scroll', myApp.scrollUp);
 
 
-// get in touch: click on get in Touch button pop up comment box.
-const commentApp = {};
-
-commentApp.facilitySection = document.querySelector('.facility-comment-form')
-commentApp.getInTouchHeader= document.querySelector('.get-in-touch')
-commentApp.closeButton = document.querySelector('.close-btn');
+// get in touch: click on get-in-Touch header or help font-awesome button to pop-up comment box.
+// create a variable and select html selector
+myApp.facilitySection = document.querySelector('.facility-comment-form')
+myApp.getInTouchHeader= document.querySelector('.get-in-touch')
+myApp.closeButton = document.querySelector('.close-btn');
+myApp.overLay = document.querySelector('#overlay')
  
-// add event listner to show comment form
-commentApp.getInTouchHeader.addEventListener("click",function(){
-    commentApp.facilitySection.classList.remove('show');
+// add an event listner to show comment-form and overlay on background
+myApp.getInTouchHeader.addEventListener("click",function(){
+    myApp.facilitySection.classList.remove('showBox');
+    myApp.overLay.classList.add('show')
 })
-// add event listner to close comment form
-commentApp.closeButton.addEventListener("click", function () {
-    commentApp.facilitySection.classList.add('show');
+// add an event listner to close comment-form and overlay background
+myApp.closeButton.addEventListener("click", function () {
+    myApp.facilitySection.classList.add('showBox');
+    myApp.overLay.classList.remove('show')
 })
                 
+myApp.init();
